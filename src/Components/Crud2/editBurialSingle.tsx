82427d36a19812908aca30f9bc611f35e749b365
tannerwoodrum <tannerwoodrum@TANNERWOODR8C05>
2023-04-14 02:45:58 -0600

    adding reroute

--- src/Components/Crud2/editBurialSingle.tsx
+++ src/Components/Crud2/editBurialSingle.tsx
@@ -20,6 +20,7 @@ function EditBurialSingle(location: any) {
       .catch((error) => {
         console.error('There was a problem with the axios operation:', error);
       });
+      window.location.href = "/tools"
   };
 
   const handleChange = (event: any) => {
