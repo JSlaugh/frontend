58b27c696d0e5aa23ec837e75a6bf6763b26c8a6
Joe Lambert <jmlambert58@gmail.com>
2023-04-14 01:15:57 -0600

    create

--- src/Components/Crud/burialForm.tsx
+++ src/Components/Crud/burialForm.tsx
@@ -1,17 +1,120 @@
-import React from 'react';
+// @ts-nocheck
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { Burialmain } from '../../Models/burialmain';
+import axios from 'axios';
+function EditBurialSingle(location: any) {
+  const [data, setData] = useState<Burialmain>();
 
-function BurialForm() {
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    console.log(formData);
+    axios
+      .put(
+        process.env.REACT_APP_BACK_END_URL + '/api/Fagelgamous/CreateBurialMain',
+        formData,
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the axios operation:', error);
+      });
+  };
+
+  const handleChange = (event: any) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+//   useEffect(() => {
+//     let temp = location.state.burialData;
+//     setData(temp);
+//     console.log(data);
+//     setFormData({
+//       id: data?.id,
+//       squarenorthsouth: data?.squarenorthsouth,
+//       burialnumber: data?.burialnumber,
+//       squareeastwest: data?.squareeastwest,
+//       northsouth: data?.northsouth,
+//       eastwest: data?.eastwest,
+//       southtohead: data?.southtohead,
+//       westtohead: data?.westtohead,
+//       westtofeet: data?.westtofeet,
+//       southtofeet: data?.southtofeet,
+//       burialnumber: data?.burialnumber,
+//       headdirection: data?.headdirection,
+//       shaftnumber: data?.shaftnumber,
+//       clusternumber: data?.clusternumber,
+//       depth: data?.depth,
+//       area: data?.area,
+//       sex: data?.sex,
+//       ageatdeath: data?.ageatdeath,
+//       adultsubadult: data?.adultsubadult,
+//       hair: data?.hair,
+//       haircolor: data?.haircolor,
+//       facebundles: data?.facebundles,
+//       preservation: data?.preservation,
+//       wrapping: data?.wrapping,
+//       samplescollected: data?.samplescollected,
+//       length: data?.length,
+//       burialmaterials: data?.burialmaterials,
+//       dataexpertinitials: data?.dataexpertinitials,
+//       fieldbookpage: data?.fieldbookpage,
+//       excavationrecorder: data?.excavationrecorder,
+//       dateofexcavation: data?.dateofexcavation,
+//       fieldbookexcavationyear: data?.fieldbookexcavationyear,
+//       burialid: data?.burialid,
+//       photos: data?.photos,
+//       mainTextiles: data?.mainTextiles,
+
+      
+//     });
+//   }, 
+//   [data, location.state.burialData]);
+
+  const [formData, setFormData] = useState({
+    id: '',
+    squarenorthsouth: '',
+    burialnumber: '',
+    squareeastwest: '',
+    northsouth: '',
+    eastwest: '',
+    southtohead: '',
+    westtohead: '',
+    westtofeet: '',
+    southtofeet: '',
+    burialnumber: '',
+    headdirection: '',
+    shaftnumber: '',
+    clusternumber: '',
+    depth: '',
+    sex: '',
+    ageatdeath: '',
+    adultsubadult: '',
+    hair: '',
+    facebundles: '',
+    preservation: '',
+    wrapping: '',
+    samplescollected: '',
+    length: '',
+    burialmaterials: '',
+    dataexpertinitials: '',
+    fieldbookexcavationyear: '',
+    burialid: '',
+    photos: '',
+    mainTextiles: '',
+  });
   return (
-    <div>
-      <h1>Burial Form</h1>
+    <div className="container">
+        <h1>Burial Form</h1>
       <br></br>
       <br></br>
-      <form method="post">
+      <form onSubmit={handleSubmit}>
         <div className="container">
           <div className="row">
             <h2>Location</h2>
             <div>
-              <input type="hidden" name="id" value="0"></input>
+              <input type="hidden" name="id" value={data?.id} onChange={handleChange} ></input>
             </div>
             <div className="form-group col">
               <label>Square North South</label>
@@ -20,6 +123,8 @@ function BurialForm() {
                 className="form-control"
                 name="squarenorthsouth"
                 placeholder="Square North South"
+                value={formData.squarenorthsouth} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -29,6 +134,8 @@ function BurialForm() {
                 className="form-control"
                 name="squareeastwest"
                 placeholder="square East West"
+                value={formData.squareeastwest} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -39,6 +146,8 @@ function BurialForm() {
                 className="form-control"
                 name="northsouth"
                 placeholder="North South"
+                value={formData.northsouth} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -48,6 +157,8 @@ function BurialForm() {
                 className="form-control"
                 name="eastwest"
                 placeholder="East West"
+                value={formData.eastwest} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -58,6 +169,8 @@ function BurialForm() {
                 className="form-control"
                 name="southtohead"
                 placeholder="South To Head"
+                value={formData.southtohead} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -67,6 +180,8 @@ function BurialForm() {
                 className="form-control"
                 name="westtohead"
                 placeholder="westtohead"
+                value={formData.westtohead} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -76,6 +191,8 @@ function BurialForm() {
                 className="form-control"
                 name="westtofeet"
                 placeholder="westtofeet"
+                value={formData.westtofeet} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -85,6 +202,8 @@ function BurialForm() {
                 className="form-control"
                 name="southtofeet"
                 placeholder="southtofeet"
+                value={formData.southtofeet} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -95,6 +214,8 @@ function BurialForm() {
                 className="form-control"
                 name="burialnumber"
                 placeholder="burialnumber"
+                value={formData.burialnumber} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -104,6 +225,8 @@ function BurialForm() {
                 className="form-control"
                 name="headdirection"
                 placeholder="Head Direction"
+                value={formData.headdirection} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -113,6 +236,8 @@ function BurialForm() {
                 className="form-control"
                 name="shaftnumber"
                 placeholder="Shaft Number"
+                value={formData.shaftnumber} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -123,6 +248,8 @@ function BurialForm() {
                 className="form-control"
                 name="clusternumber"
                 placeholder="clusternumber"
+                value={formData.clusternumber} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -132,6 +259,8 @@ function BurialForm() {
                 className="form-control"
                 name="depth"
                 placeholder="Depth"
+                value={formData.depth} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -141,6 +270,8 @@ function BurialForm() {
                 className="form-control"
                 name="area"
                 placeholder="area"
+                value={formData.area} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -155,6 +286,8 @@ function BurialForm() {
                 className="form-control"
                 name="sex"
                 placeholder="Sex"
+                value={formData.sex} 
+                onChange={handleChange}
               />
             </div>
 
@@ -165,6 +298,8 @@ function BurialForm() {
                 className="form-control"
                 name="ageatdeath"
                 placeholder="Age At Death"
+                value={formData.ageatdeath} 
+                onChange={handleChange}
               />
             </div>
 
@@ -175,6 +310,8 @@ function BurialForm() {
                 className="form-control"
                 name="adultsubadult"
                 placeholder="Adult Subadult"
+                value={formData.adultsubadult} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -184,6 +321,8 @@ function BurialForm() {
                 className="form-control"
                 name="hair"
                 placeholder="Hair"
+                value={formData.hair} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -193,6 +332,8 @@ function BurialForm() {
                 className="form-control"
                 name="haircolor"
                 placeholder="Hair Color"
+                value={formData.haircolor} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -203,6 +344,8 @@ function BurialForm() {
                 className="form-control"
                 name="facebundles"
                 placeholder="Facebundles"
+                value={formData.facebundles} 
+                onChange={handleChange}
               />
             </div>
 
@@ -213,6 +356,8 @@ function BurialForm() {
                 className="form-control"
                 name="preservation"
                 placeholder="Preservation"
+                value={formData.preservation} 
+                onChange={handleChange}
               />
             </div>
 
@@ -223,6 +368,8 @@ function BurialForm() {
                 className="form-control"
                 name="goods"
                 placeholder="Goods"
+                value={formData.goods} 
+                onChange={handleChange}
               />
             </div>
 
@@ -233,6 +380,8 @@ function BurialForm() {
                 className="form-control"
                 name="wrapping"
                 placeholder="Wrapping"
+                value={formData.wrapping} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -243,6 +392,8 @@ function BurialForm() {
                 className="form-control"
                 name="samplescollected"
                 placeholder="Samples Collected"
+                value={formData.samplescollected} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -252,6 +403,8 @@ function BurialForm() {
                 className="form-control"
                 name="length"
                 placeholder="length"
+                value={formData.length} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -261,6 +414,8 @@ function BurialForm() {
                 className="form-control"
                 name="burialmaterials"
                 placeholder="Burial Materials"
+                value={formData.burialmaterials} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -275,6 +430,8 @@ function BurialForm() {
                 className="form-control"
                 name="dataexpertinitials"
                 placeholder="Data Expert Initials"
+                value={formData.dataexpertinitials} 
+                onChange={handleChange}
               />
             </div>
 
@@ -285,6 +442,8 @@ function BurialForm() {
                 className="form-control"
                 name="fieldbookpage"
                 placeholder="Field Book Page"
+                value={formData.fieldbookpage} 
+                onChange={handleChange}
               />
             </div>
 
@@ -295,6 +454,8 @@ function BurialForm() {
                 className="form-control"
                 name="excavationrecorder"
                 placeholder="Excavation Recorder"
+                value={formData.excavationrecorder} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -305,6 +466,8 @@ function BurialForm() {
                 className="form-control"
                 name="dateofexcavation"
                 placeholder="Date of Excavation"
+                value={formData.dateofexcavation} 
+                onChange={handleChange}
               />
             </div>
             <div className="form-group col">
@@ -314,6 +477,8 @@ function BurialForm() {
                 className="form-control"
                 name="fieldbookexcavationyear"
                 placeholder="Field Book Excavation Year"
+                value={formData.fieldbookexcavationyear} 
+                onChange={handleChange}
               />
             </div>
             <div className="w-100"></div>
@@ -323,16 +488,18 @@ function BurialForm() {
                 className="form-control"
                 name="text"
                 placeholder="Text"
+                value={formData.text} 
+                onChange={handleChange}
               />
             </div>
             <div>
-              <input type="hidden" name="burialid" value=""></input>
+              <input type="hidden" name="burialid" value={data?.burialid} onChange={handleChange}></input>
             </div>
             <div>
-              <input type="hidden" name="photos" value=""></input>
+              <input type="hidden" name="photos" value={data?.photos} onChange={handleChange}></input>
             </div>
             <div>
-              <input type="hidden" name="mainTextiles" value=""></input>
+              <input type="hidden" name="mainTextiles" value={data?.mainTextiles} onChange={handleChange}></input>
             </div>
           </div>
         </div>
@@ -349,4 +516,4 @@ function BurialForm() {
   );
 }
 
-export default BurialForm;
+export default EditBurialSingle;
